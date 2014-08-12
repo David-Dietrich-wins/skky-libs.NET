c7b1891f064188f4da2cf9ae6bf873dc3266ead9
grayarrow <grayarrow@gmail.com>
2014-08-12 15:09:02 -0700

    Added ISkkyEntities to hold the repository DateTime and User name.

---
+++ skky4/Types/ISkkyEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using skky.jqGrid;
+using skky.util;
+
+namespace skky.Types
+{
+	public interface ISkkyEntities
+	{
+		DateTime SyncTime { get; }
+		string UserName { get; }
+	}
+}
