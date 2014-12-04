5cc9f5cc280ba49b7cae4739203d62bcddd058a8
grayarrow <grayarrow@gmail.com>
2014-12-04 09:11:44 -0800

    Added UrlCombine to the skky Extensions.

--- skky4/util/Extensions.cs
+++ skky4/util/Extensions.cs
@@ -711,6 +711,24 @@ namespace skky.util
 			return (str ?? string.Empty).Encode(new UnicodeEncoding());
 		}
 
+		public static string UrlCombine(this string url1, string url2)
+		{
+			if (url1.Length == 0)
+			{
+				return url2;
+			}
+
+			if (url2.Length == 0)
+			{
+				return url1;
+			}
+
+			url1 = url1.TrimEnd('/', '\\');
+			url2 = url2.TrimStart('/', '\\');
+
+			return string.Format("{0}/{1}", url1, url2);
+		}
+
 		#region LinkedResourceCollection.AddGif
 		public static void AddGif(this LinkedResourceCollection instance, Bitmap bitmap, string contentID)
 		{
