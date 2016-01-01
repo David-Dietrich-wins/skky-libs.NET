9a8218c4a6cf529962c6765f1f26c7cbc77cd304
grayarrow <grayarrow@gmail.com>
2015-12-31 21:05:55 -0800

    Added an IEntityid class to differentiate from IEntityWithId.
    We are using little id's for this project.

---
+++ skky4/Types/IEntityd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skky.Types
+{
+	public interface IEntityid<TEntityId>
+	{
+		TEntityId id { get; }
+	}
+
+	public interface IEntityIntid : IEntityid<int>
+	{ }
+
+	public interface IEntityLongid : IEntityid<long>
+	{ }
+
+	public interface IEntityGuidid : IEntityid<Guid>
+	{ }
+
+	public interface IEntityStringid : IEntityid<string>
+	{ }
+}
