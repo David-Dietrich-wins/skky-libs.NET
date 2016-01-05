0897594609564cc68f3c7edd4e13d53bd07fe559
grayarrow <grayarrow@gmail.com>
2016-01-04 17:58:11 -0800

    Added idts for wrapping a transaction id and a timestamp.
    Added ReturnStatusWithoutObject to support return statuses with different obj types.

---
+++ skky4/Types/ReturnStatusWithoutObject.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using skky.util;
+
+namespace skky.Types
+{
+	public class ReturnStatusWithoutObject : idts
+	{
+		public int code { get; set; }
+
+		public string url { get; set; }
+
+		public List<string> msg { get; set; }
+
+		public List<string> err { get; set; }
+
+		public ReturnStatusWithoutObject()
+		{
+			msg = new List<string>();
+			err = new List<string>();
+		}
+		public ReturnStatusWithoutObject(int rc)
+			: this()
+		{
+			code = rc;
+		}
+		public ReturnStatusWithoutObject(int rc, string errorMessage)
+			: this(rc)
+		{
+			err.Add(errorMessage);
+		}
+		public ReturnStatusWithoutObject(string errorMessage)
+			: this()
+		{
+			err.Add(errorMessage);
+		}
+
+		public bool HasErrors()
+		{
+			return (code < 0 || err.Any());
+		}
+		public bool ErrorFree()
+		{
+			return !HasErrors();
+		}
+		public bool HasMessages()
+		{
+			return msg.Any();
+		}
+		public bool HasAnyMessages()
+		{
+			return HasErrors() || HasMessages();
+		}
+
+		public int AddMessages(ReturnStatusWithoutObject rs)
+		{
+			int messagesAdded = 0;
+			if (null != rs)
+			{
+				foreach (var rsErr in rs.err)
+				{
+					err.Add(rsErr);
+					++messagesAdded;
+				}
+
+				foreach (var rsMsg in rs.msg)
+				{
+					msg.Add(rsMsg);
+					++messagesAdded;
+				}
+			}
+
+			return messagesAdded;
+		}
+
+		public string GetErrorMessage()
+		{
+			if (HasErrors())
+				return string.Join(", ", err);
+
+			return string.Empty;
+		}
+
+		public void AddError(string errorString)
+		{
+			err.Add(errorString);
+		}
+
+		public void AddMessage(string message)
+		{
+			msg.Add(message);
+		}
+	}
+}
---
+++ skky4/Types/idts.cs
@@ -0,0 +1,63 @@
+﻿using skky.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skky.Types
+{
+	public class idts
+	{
+		private static object m_TransactionObject = new object();
+		private static long m_TransactionId = 0;
+
+		private long m_id = 0;
+		/// <summary>
+		/// Transaction ID for this object.
+		/// </summary>
+		public long id
+		{
+			get
+			{
+				if (m_id == 0)
+					m_id = GetNextId();
+
+				return m_id;
+			}
+			set
+			{
+				m_id = value;
+			}
+		}
+
+		private long m_ts = 0;
+		/// <summary>
+		/// Unix Timestamp in milliseconds since Unix Epoch 1/1/1970.
+		/// </summary>
+		public long ts
+		{
+			get
+			{
+				if (m_ts == 0)
+					m_ts = DateTimeHelper.GetUnixTimestampMillis();
+
+				return m_ts;
+			}
+			set
+			{
+				m_ts = value;
+			}
+		}
+
+		protected long GetNextId()
+		{
+			lock(m_TransactionObject)
+			{
+				++m_TransactionId;
+
+				return m_TransactionId;
+			}
+		}
+	}
+}
